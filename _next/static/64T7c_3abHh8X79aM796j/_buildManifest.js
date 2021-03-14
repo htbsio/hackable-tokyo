910c3224bc17ef91aa57dac45609aadb181c6368
wktq <wktq@users.noreply.github.com>
2021-03-14 17:01:17 +0000

    deploy: a45a3ae87908ab34b7c555656489bd6ce3a2f2ed

---
+++ _next/static/64T7c_3abHh8X79aM796j/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F2a760f1ea6b9234d869a.css","static\u002Fchunks\u002Fpages\u002Findex-a03836e9091587781f22.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-2f78d3a5f3f9c844e954.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
